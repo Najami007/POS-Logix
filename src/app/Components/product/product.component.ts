1f1a3b07966b75c94544ec123ffe8ba6af88d355
Najami007 <najamali244@gmail.com>
2023-06-09 16:23:16 +0500

    first commit

--- src/app/Components/product/product.component.ts
+++ src/app/Components/product/product.component.ts
@@ -1,5 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { GlobalDataModule } from 'src/app/Shared/global-data/global-data.module';
+import { environment } from 'src/environments/environment.development';
 
 @Component({
   selector: 'app-product',
@@ -8,10 +10,86 @@ import { GlobalDataModule } from 'src/app/Shared/global-data/global-data.module'
 })
 export class ProductComponent implements OnInit{
 
-  constructor(private globalData:GlobalDataModule){}
+  constructor(private globalData:GlobalDataModule,
+    private http:HttpClient
+    ){}
 
   ngOnInit(): void {
     this.globalData.setHeaderTitle('Add Product');
+    this.getCategory();
+    this.getSubCategory();
+    
+    
   }
 
+
+  CategoriesNames:any;
+  SubCategoriesNames:any;
+  hide= true;
+
+  myCategoryID:any;
+  mySubCategoryID:any;
+  myProductName:any;
+  myBarcodeType= 'auto';
+  myBarcode:any;
+  myBarcode1:any;
+  myBarcode2:any;
+  myBarcode3:any;
+  myCostPrice:any;
+  myCTCPrice:any;
+  myWholeSalePrice:any;
+  myRetailPrice:any;
+  myGst:any;
+  myUOM:any;
+
+  // ChangeType(){
+  //   if(this.myBarcodeType == 1){
+  //     this.hide = true;
+  //   }else{
+  //     this.hide = false;
+  //   }
+  // }
+
+  getCategory(){
+    this.http.get(environment.apiUrl+'api/productCategory/getcategory',{responseType:'json'}).subscribe({
+      next:value=>{
+        this.CategoriesNames = value;
+      },
+      error:error=>{
+        console.log(error);
+      }
+    })
+  }
+
+
+  getSubCategory(){
+    this.http.get(environment.apiUrl+'api/productSubCategory/getsubcategory').subscribe({
+      next:value=>{
+        this.SubCategoriesNames = value;
+      },
+      error:error=>{
+       
+        console.log(error);
+      }
+    })
+  }
+
+
+
+
+  reset(){
+    this.myCategoryID='';
+    this.mySubCategoryID='';
+    this.myProductName = '';
+    this.myBarcode='';
+    this.myBarcode1='';
+    this.myBarcode2='';
+    this.myBarcode3='';
+    this.myCostPrice='';
+    this.myCTCPrice='';
+    this.myWholeSalePrice='';
+    this.myRetailPrice= '';
+    this.myGst = '';
+    this.myUOM = '';
+  }
 }
